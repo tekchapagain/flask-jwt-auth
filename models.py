a69dd88872f4821bc8d32f69dd8c5dee97f31a0b
Tek Prasad Chapagain <tekprasadc@infinite.com>
2024-07-04 22:55:22 +0545

    Fixed unique id generation for each user

--- models.py
+++ models.py
@@ -1,15 +1,11 @@
 from extension import db
-from uuid import uuid4
+import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-
-def generate_uuid():
-    return uuid4()
-    
-
+ 
 class User(db.Model):
     __tablename__ = "users"
-    id = db.Column(db.String(), primary_key=True, default=str(generate_uuid()))
+    id = db.Column(db.String(), primary_key=True, default=lambda:str(uuid.uuid4()))
     username = db.Column(db.String(), nullable=False)
     email = db.Column(db.String(), nullable=False)
     password = db.Column(db.Text())
