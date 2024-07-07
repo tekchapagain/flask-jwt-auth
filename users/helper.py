6d31dd1d0f0bd98283f783db077453add2352849
Tek Prasad Chapagain <tekprasadc@infinite.com>
2024-07-07 19:28:58 +0545

    Fixed minor issue

--- users/helper.py
+++ users/helper.py
@@ -18,5 +18,5 @@ def send_forgot_password_email(request, user):
         mail_subject, sender="info@niceclickllc.com", recipients=[user.email]
     )
     msg.html = f"Please click on the link to reset your password, {current_site}api/auth/reset-password/{token}/"
-    print(msg.html)
-    # mail.send(msg)
+    # print(msg.html)
+    mail.send(msg)
